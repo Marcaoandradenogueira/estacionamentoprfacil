0bc647ed733c75ab89dad9ade72636bc108e9ddc
unknown <marcaoandradenogueira@gmail.com>
2019-06-21 12:15:46 -0300

    Segundo commit

--- core/admin.py
+++ core/admin.py
@@ -4,15 +4,24 @@ from .models import (
     Veiculo,
     Pessoa,
     Parametros,
-    MovRotativo
+    MovRotativo,
+    Mensalista,
+    MovMensalista
 )
     
 class MovRotativoAdmin(admin.ModelAdmin):
     list_display = (
-        'checkin', 'checkout', 'valor_hora', 'pago', 'horas_total')
+        'checkin', 'checkout', 'valor_hora', 'pago', 'horas_total','veiculo')
+
+
+class MovMensalistaAdmin(admin.ModelAdmin):
+    list_display = ('mensalista', 'dt_pgto', 'total')
+
 
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
+admin.site.register(Mensalista)
+admin.site.register(MovMensalista, MovMensalistaAdmin)
 admin.site.register(MovRotativo, MovRotativoAdmin)
